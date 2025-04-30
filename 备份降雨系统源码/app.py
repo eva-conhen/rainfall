9c6ed343583989a027ab6eada93bb11afa76f2bd
eva-conhen <3190735604@qq.com>
2025-04-30 17:21:24 +0800

    Add files via upload

--- 备份降雨系统源码/app.py
+++ 备份降雨系统源码/app.py
@@ -688,4 +688,5 @@ if __name__ == '__main__':
     updater_thread.start()
     
     # 启动应用程序
-    app.run(debug=True, port=5001)
+    # app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=False)
